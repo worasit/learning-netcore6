5090c1d83cc0f251a9b833c047581ed387434978
worasit <worasit.daimongkol@thoughtworks.com>
2022-08-12 06:48:24 +0700

    Introduced productAPI service in frontend

---
+++ MangoRestaurant/Mango.Web/Models/ResponseDto.cs
@@ -0,0 +1,9 @@
+namespace Mango.Web.Models;
+
+public class ResponseDto
+{
+    public bool isSuccess { get; set; } = true;
+    public object Result { get; set; }
+    public string DisplayMessage { get; set; } = "";
+    public List<string> ErrorMessage { get; set; }
+}
